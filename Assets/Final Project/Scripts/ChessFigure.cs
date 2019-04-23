e29ab8c021bc8c20c0b1683a306958751e4de4ac
NikolajX4000 <supernikolaj@hotmail.com>
2019-04-23 12:21:34 +0200

    initial commit

---
+++ Assets/Final Project/Scripts/ChessFigure.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public abstract class ChessFigure : MonoBehaviour {
+    public int CurrentX { get; set; }
+    public int CurrentY { get; set; }
+    public bool isWhite;
+
+    public void SetPosition(int x, int y) {
+        CurrentX = x;
+        CurrentY = y;
+    }
+
+    public virtual bool[,] PossibleMove() {
+        return new bool[3, 3];
+    }
+}
---
+++ Assets/Final Project/Scripts/Pawn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : ChessFigure {
+    public override bool[,] PossibleMove() {
+        bool[,] r = new bool[3, 3];
+        ChessFigure c, c2;
+
+        if (isWhite) {
+            // Diagonal Left
+            if (CurrentX != 0 && CurrentY != 2) {
+                c = BoardManager.Instance.ChessFigurePositions[CurrentX - 1, CurrentY + 1];
+                if (c != null && !c.isWhite) {
+                    r[CurrentX - 1, CurrentY + 1] = true;
+                }
+            }
+            // Diagonal Right
+            if (CurrentX != 2 && CurrentY != 2) {
+                c = BoardManager.Instance.ChessFigurePositions[CurrentX + 1, CurrentY + 1];
+                if (c != null && !c.isWhite) {
+                    r[CurrentX + 1, CurrentY + 1] = true;
+                }
+            }
+            // Forward
+            if (CurrentY != 2) {
+                c = BoardManager.Instance.ChessFigurePositions[CurrentX, CurrentY + 1];
+                if (c == null) {
+                    r[CurrentX, CurrentY + 1] = true;
+                }
+            }
+        } else {
+            // Diagonal Left
+            if (CurrentX != 0 && CurrentY != 0) {
+                c = BoardManager.Instance.ChessFigurePositions[CurrentX - 1, CurrentY - 1];
+                if (c != null && c.isWhite) {
+                    r[CurrentX - 1, CurrentY - 1] = true;
+                }
+            }
+            // Diagonal Right
+            if (CurrentX != 2 && CurrentY != 0) {
+                c = BoardManager.Instance.ChessFigurePositions[CurrentX + 1, CurrentY - 1];
+                if (c != null && c.isWhite) {
+                    r[CurrentX + 1, CurrentY - 1] = true;
+                }
+            }
+            // Forward
+            if (CurrentY != 0) {
+                c = BoardManager.Instance.ChessFigurePositions[CurrentX, CurrentY - 1];
+                if (c == null) {
+                    r[CurrentX, CurrentY - 1] = true;
+                }
+            }
+        }
+        return r;
+    }
+}
