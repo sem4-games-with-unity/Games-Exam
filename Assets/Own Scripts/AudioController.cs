c591b2ed9f456173368c5108d685d89b0f2c504e
Nikolaj <supernikolaj@hotmail.com>
2019-05-25 00:46:44 +0200

    Added more sounds

--- Assets/Own Scripts/AudioController.cs
+++ Assets/Own Scripts/AudioController.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class AudioController : MonoBehaviour {
-    public AudioClip StartClip;
-    public AudioClip LoopClip;
+    public AudioClip winClip;
+    public AudioClip loseClip;
+    public AudioClip loopClip;
 
     void Start() {
         StartCoroutine(playSound());
     }
 
     IEnumerator playSound() {
-        GetComponent<AudioSource>().clip = StartClip;
-        GetComponent<AudioSource>().Play();
-        yield return new WaitForSeconds(StartClip.length / 2);
-        GetComponent<AudioSource>().clip = LoopClip;
+        if (GameDetails.GameMode == GameDetails.Mode.Singleplayer && GameDetails.Winner == GameDetails.Team.White) {
+            GetComponent<AudioSource>().clip = winClip;
+            GetComponent<AudioSource>().Play();
+            yield return new WaitForSeconds(winClip.length / 2);
+        }
+        if (GameDetails.GameMode == GameDetails.Mode.Singleplayer && GameDetails.Winner == GameDetails.Team.Black) {
+            GetComponent<AudioSource>().clip = loseClip;
+            GetComponent<AudioSource>().Play();
+            yield return new WaitForSeconds(loseClip.length / 2);
+        }
+        GetComponent<AudioSource>().clip = loopClip;
         GetComponent<AudioSource>().Play();
         GetComponent<AudioSource>().loop = true;
     }
